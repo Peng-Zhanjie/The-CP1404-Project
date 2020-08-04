34d62591cd9baf413f2005db76833975ad435d58
Peng-Zhanjie <nowarzenx@gmail.com>
2020-08-05 00:40:26 +0800

    Do-from-scratch_Exercises_scores

--- Week3/broken_score.py
+++ Week3/broken_score.py
@@ -12,11 +12,16 @@ def Score_Calculation(score):
       if score < 50:
         print("{0} is Bad".format(score))
 
+def Randomprogram():
+    Randomone = random.randint(1, 100)
+    return Randomone
+
+
 def main():
     score = float(input("Enter score: "))
     Score_Calculation(score)
-    Therandom=random.randint(1,100)
-    print("The random program:")
+    Therandom=Randomprogram()
+    print("The random program output below:")
     Score_Calculation(Therandom)
 
 main()
