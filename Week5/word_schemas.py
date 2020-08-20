fec475ae0731ae424d46ea2f8c7d4a0041045a8d
Peng-Zhanjie <nowarzenx@gmail.com>
2020-08-20 16:03:55 +0800

    Schemas

---
+++ Week5/word_schemas.py
@@ -0,0 +1,10 @@
+Record={}W
+Words=input("Text:")
+Words=Words.split(' ')
+length=0
+for line in Words:
+    if len(line)>=length:length=len(line)
+    if line in Record.keys(): Record[line]+=1
+    else:Record[line]=1
+for line2 in Record:
+    print("{:{}}".format(line2,length),":","{}".format(Record[line2]))
